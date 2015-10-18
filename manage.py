b30ad3a59aba92e895c08de4587e0a5bee00e1c0
Michael Timpelan <michael.timpelan@fim.uni-mannheim.de>
2015-10-19 00:56:51 +0200

    added libraries

--- manage.py
+++ manage.py
@@ -1,16 +1,19 @@
 from __future__ import unicode_literals
-__author__ = 'lexxodus'
-
+from flask_collect import Collect
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
-import os
-
 from app import app, db
+
+__author__ = 'lexxodus'
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+collect = Collect()
+collect.init_app(app)
+collect.init_script(manager)
 
 if __name__ == '__main__':
     manager.run()
